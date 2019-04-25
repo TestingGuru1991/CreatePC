44b664bac9a2b30227aa51749c0f1f1f0a376681
TestingGuru1991 <41591445+TestingGuru1991@users.noreply.github.com>
2019-04-25 20:31:17 +0200

    Add files via upload

---
+++ CreatePC/src/Motherboard.java
@@ -0,0 +1,40 @@
+public class Motherboard {
+
+    private String model;
+    private String manufacturer;
+    private String ramSlots;
+    private int cardSlots;
+    private String bios;
+
+    public Motherboard(String model, String manufacturer, String ramSlots, int cardSlots, String bios) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.ramSlots = ramSlots;
+        this.cardSlots = cardSlots;
+        this.bios = bios;
+    }
+
+    public void loadProgram(String programName) {
+        System.out.println("Program " + programName + " is now loading...");
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getRamSlots() {
+        return ramSlots;
+    }
+
+    public int getCardSlots() {
+        return cardSlots;
+    }
+
+    public String getBios() {
+        return bios;
+    }
+}
